d5f35efb47686f32ec62414a6acf53427fff3689
FokinAleksandr <shwayze@yandex.ru>
2021-12-02 06:07:12 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ echo "allow apps downloaded from anywhere"
 sudo spctl --master-disable
 
 echo "installing a few global npm packages"
-npm install --global parcel-bundler fkill-cli yarn
+npm install --global fkill-cli yarn
 
 echo "installing apps with brew cask"
 brew install --cask discord
